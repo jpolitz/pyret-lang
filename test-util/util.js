07b735f9d7dcc45935985064dcc8b9f81b4ec380
Joe Politz <joe.politz@gmail.com>
2026-05-23 07:29:11 -0700

    Revert "Claude tries to fix failing tests"
    This reverts commit 577121516a26652a0c0189be4dd68f102e29ac24.

--- test-util/util.js
+++ test-util/util.js
@@ -25,7 +25,6 @@ let leave_open = process.env.LEAVE_OPEN === "true" || false;
 let args = process.env.SHOW_BROWSER ? [] : [
   '--headless',
   '--no-sandbox',
-  '--disable-dev-shm-usage',
 ];
 if(!process.env.SHOW_BROWSER) {
   console.log("Running Chrome headless. You can set SHOW_BROWSER=true to see what's going on");
